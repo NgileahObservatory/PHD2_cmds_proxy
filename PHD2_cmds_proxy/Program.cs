8f0ed0c7bd02cfb725947c37822fd3da84afb3ec
Carl Knight <carl.knight@fingertapps.com>
2018-03-30 23:09:06 +0000

    Initial source files.

---
+++ PHD2_cmds_proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 
+
+namespace PHD2_cmds_proxy
+{
+   class Program
+   {
+      new OptionSet      
+      /// <summary>
+      /// Arguments are as follows:
+      /// 
+      /// --script={filename}
+      /// --command={string}
+      /// </summary>
+      /// <param name="args"></param>
+      static void Main(string[] args)
+      {
+      }
+   }
+}
