f42a2cfcb667221c144533671bd52f63a109bb3d
Carl Knight <carl.knight@fingertapps.com>
2018-04-01 21:53:40 +0000

    Basic working code. Supports single command and response.

--- PHD2_cmds_proxy/PHD2Client.cs
+++ PHD2_cmds_proxy/PHD2Client.cs
@@ -2,7 +2,8 @@
 using System.Net;  
 using System.Net.Sockets;  
 using System.Threading;  
-using System.Text;  
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PHD2_cmds_proxy
 {
@@ -16,100 +17,166 @@ namespace PHD2_cmds_proxy
        // Receive buffer.  
        public byte[] buffer = new byte[BufferSize];  
        // Received data string.  
-       public StringBuilder sb = new StringBuilder();  
+       public StringBuilder sb = new StringBuilder();
+  
+      public Action<StateObject> nextAction = null;
+
+      public StateObject(Action<StateObject> nextAction)
+      {
+         this.nextAction = nextAction;
+      }
+
+      public StateObject()
+      {
+      }
    }
 
-   public class AsynchronousClient
+   class PHD2Client
    {
-      // The port number for the PHD/2 instance.  
-      private const int port = 4400;
+      private Socket socket = null; 
 
-      // ManualResetEvent instances signal completion.  
-      private static ManualResetEvent connectDone =
-          new ManualResetEvent(false);
-      private static ManualResetEvent sendDone =
-          new ManualResetEvent(false);
-      private static ManualResetEvent receiveDone =
-          new ManualResetEvent(false);
+      /// <summary>
+      /// Constructor where json scripted commands and responses are in a file.
+      /// </summary>
+      /// <param name="host"></param>
+      /// <param name="port"></param>
+      public PHD2Client(string host, int port)
+      {
+         StartClient(host, port);
+      }
 
-      // The response from the remote device.  
-      private static String response = String.Empty;
-
-      private static void StartClient()
+      private void StartClient(string host, int port)
       {
          // Connect to a remote device.  
-         try
+         // Establish the remote endpoint for the socket.  
+         // The name of the   
+         // remote device is "localhost".  
+         IPAddress[] ipAddresses = Dns.GetHostAddresses(host);
+         if (ipAddresses.Length == 0)
          {
-            // Establish the remote endpoint for the socket.  
-            // The name of the   
-            // remote device is "localhost".  
-            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-
-            // Create a TCP/IP socket.  
-            Socket client = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect to the remote endpoint.  
-            client.BeginConnect(remoteEP,
-                new AsyncCallback(ConnectCallback), client);
-            connectDone.WaitOne();
-
-            // Send test data to the remote device.  
-            Send(client, "This is a test<EOF>");
-            sendDone.WaitOne();
-
-            // Receive the response from the remote device.  
-            Receive(client);
-            receiveDone.WaitOne();
-
-            // Write the response to the console.  
-            Console.WriteLine("Response received : {0}", response);
-
-            // Release the socket.  
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
-
+            throw new IndexOutOfRangeException("Zero IP addresses returned for given host " + host);
          }
-         catch (Exception e)
+
+         IPAddress firstIpAddress = ipAddresses[0];
+         IPEndPoint remoteEP = new IPEndPoint(firstIpAddress, port);
+
+         // Create a TCP/IP socket.  
+         socket = new Socket(firstIpAddress.AddressFamily,
+               SocketType.Stream, ProtocolType.Tcp);
+
+         // Connect to the remote endpoint.  
+         socket.BeginConnect(remoteEP,
+               new AsyncCallback(ConnectCallback), new StateObject(
+         (StateObject parent) =>
          {
-            Console.WriteLine(e.ToString());
+            this.Receive(new StateObject(
+               (StateObject state) =>
+               {
+                  isHandshakeComplete = state.sb.ToString().Contains("\"Event\":\"AppState\"");
+                  if (isHandshakeComplete)
+                  {
+                     // Handshake is complete. There is no next action.
+                     state.nextAction = null;
+                  }
+               }));
+         }));
+      }
+
+      public ManualResetEvent connectedEvent = new ManualResetEvent(false);
+      private bool connected = false;
+      public bool isConnected
+      {
+         get
+         {
+            return connected;
+         }
+         set
+         {
+            connected = value;
+            if (connected)
+            {
+               connectedEvent.Set();
+            }
          }
       }
 
-      private static void ConnectCallback(IAsyncResult ar)
+      private void ConnectCallback(IAsyncResult ar)
       {
          try
          {
             // Retrieve the socket from the state object.  
-            Socket client = (Socket)ar.AsyncState;
+            StateObject onConnect = (StateObject) ar.AsyncState;
 
             // Complete the connection.  
-            client.EndConnect(ar);
+            socket.EndConnect(ar);
 
-            Console.WriteLine("Socket connected to {0}",
-                client.RemoteEndPoint.ToString());
+            if (Program.verbose)
+            {
+               Console.WriteLine("Socket connected to {0}",
+                   socket.RemoteEndPoint.ToString());
+            }
 
-            // Signal that the connection has been made.  
-            connectDone.Set();
+            isConnected = true;
+            // Do whatever was asked on connect.
+            onConnect.nextAction(onConnect);
          }
          catch (Exception e)
          {
+            isDone = true;
             Console.WriteLine(e.ToString());
          }
       }
 
-      private static void Receive(Socket client)
+      private int tx = 0;
+      public int progressTx
+      {
+         get
+         {
+            return tx;
+         }
+         set
+         {
+            tx = value;
+         }
+      }
+
+      private int rx = 0;
+      public int progressRx
+      {
+         get
+         {
+            return rx;
+         }
+         set
+         {
+            rx = value;
+         }
+      }
+
+      public ManualResetEvent doneEvent = new ManualResetEvent(false);
+      private bool done = false;
+      public bool isDone
+      {
+         get
+         {
+            return done;
+         }
+         set
+         {
+            done = value;
+            if (done)
+            {
+               doneEvent.Set();
+            }
+         }
+      }
+
+      private void Receive(StateObject state)
       {
          try
          {
-            // Create the state object.  
-            StateObject state = new StateObject();
-            state.workSocket = client;
-
             // Begin receiving the data from the remote device.  
-            client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+            socket.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
                 new AsyncCallback(ReceiveCallback), state);
          }
          catch (Exception e)
@@ -118,36 +185,53 @@ namespace PHD2_cmds_proxy
          }
       }
 
-      private static void ReceiveCallback(IAsyncResult ar)
+      public ManualResetEvent handshakeCompleteEvent = new ManualResetEvent(false);
+      private bool handshakeComplete = false;
+      public bool isHandshakeComplete
+      {
+         get 
+         { 
+            return handshakeComplete; 
+         }
+         set 
+         { 
+            handshakeComplete = value; 
+            if (handshakeComplete)
+            {
+               handshakeCompleteEvent.Set();
+            }
+         }
+      }
+
+      private void ReceiveCallback(IAsyncResult ar)
       {
          try
          {
             // Retrieve the state object and the client socket   
             // from the asynchronous state object.  
             StateObject state = (StateObject)ar.AsyncState;
-            Socket client = state.workSocket;
 
             // Read data from the remote device.  
-            int bytesRead = client.EndReceive(ar);
+            int bytesRead = socket.EndReceive(ar);
 
             if (bytesRead > 0)
             {
                // There might be more data, so store the data received so far.  
                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
 
-               // Get the rest of the data.  
-               client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                   new AsyncCallback(ReceiveCallback), state);
-            }
-            else
-            {
-               // All the data has arrived; put it in response.  
-               if (state.sb.Length > 1)
+               rx += bytesRead;
+               if (Program.verbose)
                {
-                  response = state.sb.ToString();
+                  System.Console.WriteLine(state.sb.ToString());
                }
-               // Signal that all bytes have been received.  
-               receiveDone.Set();
+               state.nextAction(state);
+
+               if (state.nextAction != null)
+               {
+                  // Get the rest of the data.  
+                  socket.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                      new AsyncCallback(ReceiveCallback), state);
+               }
             }
          }
          catch (Exception e)
@@ -156,29 +240,59 @@ namespace PHD2_cmds_proxy
          }
       }
 
-      private static void Send(Socket client, String data)
+      public void send(string command, string response)
       {
-         // Convert the string data to byte data using ASCII encoding.  
-         byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-         // Begin sending the data to the remote device.  
-         client.BeginSend(byteData, 0, byteData.Length, 0,
-             new AsyncCallback(SendCallback), client);
+         Send(socket, command, new StateObject((StateObject parent) => 
+         {
+            if(String.IsNullOrEmpty(response))
+            {
+               // No response means we want to send and exit.
+               parent.nextAction = null;
+               isDone = true;
+            }
+            else
+            {
+               // Otherwise we want to keep receiving until 
+               // we get the PHD/2 response we are looking for.
+               Receive(new StateObject((StateObject state) =>
+               {
+                  Regex responseRegex = new Regex(response, RegexOptions.IgnoreCase);
+                  if (responseRegex.Matches(state.sb.ToString()).Count > 0)
+                  {
+                     parent.nextAction = null;
+                     isDone = true;
+                  }
+               }));
+            }
+         }));
       }
 
-      private static void SendCallback(IAsyncResult ar)
+      private void Send(Socket client, String data, StateObject state)
+      {
+         // Convert the string data to byte data using ASCII encoding.
+         if (!data.EndsWith("\n"))
+            data += "\n";
+         byte[] byteData = Encoding.ASCII.GetBytes(data);
+         if (Program.verbose)
+            System.Console.WriteLine("Sending " + data + " to server.");
+         // Begin sending the data to the remote device.  
+         client.BeginSend(byteData, 0, byteData.Length, 0,
+             new AsyncCallback(SendCallback), state);
+      }
+
+      private void SendCallback(IAsyncResult ar)
       {
          try
          {
-            // Retrieve the socket from the state object.  
-            Socket client = (Socket)ar.AsyncState;
+            StateObject state = (StateObject)ar.AsyncState;
 
             // Complete sending the data to the remote device.  
-            int bytesSent = client.EndSend(ar);
-            Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+            int bytesSent = socket.EndSend(ar);
+            if (Program.verbose)
+               Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+            tx += bytesSent;
 
-            // Signal that all bytes have been sent.  
-            sendDone.Set();
+            state.nextAction(state);
          }
          catch (Exception e)
          {
--- PHD2_cmds_proxy/Program.cs
+++ PHD2_cmds_proxy/Program.cs
@@ -3,22 +3,149 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
+using Mono.Options;
 
 namespace PHD2_cmds_proxy
 {
    class Program
    {
-      new OptionSet      
       /// <summary>
-      /// Arguments are as follows:
-      /// 
-      /// --script={filename}
-      /// --command={string}
+      /// Generate extra logging output to the console.
       /// </summary>
-      /// <param name="args"></param>
+      public static bool verbose = false;
+      /// <summary>
+      /// Display the usage.
+      /// </summary>
+      private static bool showHelp = false;
+      /// <summary>
+      /// Single json string command to send verbatim to PHD/2.
+      /// </summary>
+      private static string command;
+      /// <summary>
+      /// Single regex json response to expect in response to the command sent. If command is specified, this need not be.
+      /// </summary>
+      private static string response;
+
+      /// <summary>
+      /// TCP/IP hostname.
+      /// </summary>
+      private static string host = "127.0.0.1";
+
+      /// <summary>
+      /// TCP/IP port to connect to on host.
+      /// </summary>
+      private static int port = 4400;
+
+      private static OptionSet cmdOptions = new OptionSet
+      {
+		   { "v|verbose", "Get extra spammy output including step by step logging of communications.", v => { verbose = (v != null); } },
+		   { "c|command=", "Quoted json command to send verbatim to PHD/2. Cannot be specified with --script. --response is optional with --command.",   v => { command = v; } },
+		   { "r|response=", "Regular expression that matches json response from PHD/2 that indicates success. Requires --command be specified.",   v => { response = v; } },
+		   { "h|host=", "Optional host to connect to. Default is localhost if none specified.",   v => { host = v; } },
+		   { "p|port=", "Optional port on host to connect to. Default is port 4400 if none specified.",   v => { port = int.Parse(v); } },
+		   { "?|help", "Display this help.",  v => { showHelp = v != null; } },
+   	};
+
+      private static void ShowHelp()
+      {
+         Console.WriteLine("Usage: PHD2_cmds_proxy {--verbose|help} [--script=filename|--command=\"json string\" {--response=\"json string\"}] {--host=hostname {--port=number}}");
+         Console.WriteLine("Send commands over TCP/IP to PHD/2 and optionally handle responses.");
+         Console.WriteLine();
+         Console.WriteLine("Options:");
+         cmdOptions.WriteOptionDescriptions(Console.Out);
+      }
+
+      /// <summary>
+      /// Validate combinations of command line options.
+      /// </summary>
+      /// <param name="decodedOptions"></param>
+      /// <returns></returns>
+      private static bool argsAreValid()
+      {
+         if (String.IsNullOrEmpty(command))
+         {
+            return false;
+         }
+         else
+            return true;
+      }
+
       static void Main(string[] args)
       {
+         List<string> unknowns = new List<string>();
+         try
+         {
+            unknowns = cmdOptions.Parse(args);
+         }
+         catch(OptionException e)
+         {
+            System.Console.WriteLine("Invalid parameter(s).");
+            System.Console.WriteLine(e.Message);
+            System.Console.WriteLine("Try --help for options.");
+            return;
+         }
+
+         if (showHelp || unknowns.Count > 0 || !argsAreValid())
+         {
+            if (unknowns.Count > 0)
+            {
+               System.Console.WriteLine("Unknown options found:");
+               foreach(string opt in unknowns)
+               {
+                  System.Console.WriteLine(opt);
+               }
+            }
+            ShowHelp();
+         }
+         else
+         {
+            if (verbose)
+            {
+               System.Console.WriteLine("Args are all valid.");
+            }
+
+            PHD2Client client = new PHD2Client(host, port);
+            if (verbose)
+            {
+               System.Console.WriteLine("Connecting to " + host + ":" + port.ToString());
+            }
+
+            while (!client.isConnected && !client.isDone)
+            {
+               if (client.connectedEvent.WaitOne(1000))
+                  break;
+            }
+
+            if (!client.isDone)
+            {
+               while (!client.isHandshakeComplete && !client.isDone)
+               {
+                  if (client.handshakeCompleteEvent.WaitOne(1000))
+                     break;
+               }
+
+               if (!client.isDone)
+               {
+                  if (verbose && client.isHandshakeComplete)
+                  {
+                     System.Console.WriteLine("Handshake complete...");
+                  }
+
+                  client.send(command, response);
+
+                  while (!client.isDone)
+                  {
+                     if (client.doneEvent.WaitOne(1000))
+                        break;
+                     if (verbose)
+                     {
+                        System.Console.Write("<=>");
+                        System.Console.WriteLine("Progress Tx=" + client.progressTx.ToString() + " Rx=" + client.progressRx.ToString());
+                     }
+                  }
+               }
+            }
+         }
       }
    }
 }
